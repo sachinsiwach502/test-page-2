b0e15348254ff1527c90d4954229bcc13d7bd6d0
sachinsiwach502 <sachinsiwach502@Gmail.com>
2024-06-03 11:13:28 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,95 @@
+// nav bar----------------
+let body = document.body;
+let menuIcon = document.querySelector(".menuIcon");
+let menuList = document.querySelector(".menuBar");
+let line_1 = document.querySelector(".first-line")
+let line_2 = document.querySelector(".sec-line");
+let line_3 = document.querySelector(".third-line");
+
+function cross() {
+    line_2.classList.add("d-none");
+    // line_1.classList.add("transform-origin-center-center");
+    line_1.classList.add("position-relative");
+    line_1.style.top = "12px";
+    line_1.style.left = "2px";
+    line_1.style.transform = "rotate(45deg)"
+    line_3.classList.add("position-relative");
+    line_3.style.top = "-12px";
+    line_3.style.transform = "rotate(-45deg)";
+    line_1.classList.add("transition");
+    line_3.classList.add("transition");
+
+}
+function line() {
+    line_2.classList.remove("d-none");
+    line_1.classList.remove("position-relative");
+    line_1.style.transform = "rotate(0deg)"
+    line_3.classList.remove("position-relative");
+    line_3.style.transform = "rotate(0deg)";
+}
+
+
+// add show class in nav links--------------
+menuIcon.addEventListener("click", () => {
+    menuList.classList.toggle("show");
+    if (menuList.classList.contains("show")) {
+        cross();
+        body.style.overflow = "hidden";
+    } else {
+        line();
+        body.style.overflow = "auto";
+    }
+});
+
+
+
+// navlinks hidden show class----------
+let navLinks = document.querySelectorAll(".nav_links")
+
+navLinks.forEach(links => {
+    links.addEventListener("click", () => {
+        menuList.classList.remove("show");
+        line();
+        body.style.overflow = "auto";
+    });
+});
+
+
+// slick-slider=====================
+$('.slick_slider').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: false,
+    nextArrow: false,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 2000,
+    cssEase: 'linear',
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
